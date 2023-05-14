7080afb4c3d3b132a48a85bba2dc616841acd39e
omid milanifard <milanifard.o@gmail.com>
2023-05-14 08:01:27 +0430

    fix bug

--- pm/CoOccuranceAnalysis.php
+++ pm/CoOccuranceAnalysis.php
@@ -7,69 +7,84 @@
 include("header.inc.php");
 include_once("../sharedClasses/SharedClass.class.php");
 HTMLBegin();
-$mysql = pdodb::getInstance();
-$res = $mysql->Execute("select distinct TermReferenceID, PageNum, ParagraphNo from projectmanagement.TermReferenceMapping where TermReferenceID=1 ");
-$frequency = $docs = $terms = array();
-$i = 0;
-while($rec = $res->fetch())
+
+function Calculate($mysql, $TermRefrenceID)
 {
-  $docs[$i]["PageNum"] = $rec["PageNum"];
-  $docs[$i]["TermReferenceID"] = $rec["TermReferenceID"];
-  $docs[$i]["ParagraphNo"] = $rec["ParagraphNo"];
-  $i++;
-}
-$res = $mysql->Execute("select distinct TermID, TermTitle from projectmanagement.terms 
-JOIN projectmanagement.TermReferenceMapping using (TermID) 
-where TermReferenceID=1");
-$i = 0;
-while($rec = $res->fetch())
-{
-  $terms[$i]["TermID"] = $rec["TermID"];
-  $terms[$i]["TermTitle"] = $rec["TermTitle"];
-  $i++;
-}
-$res = $mysql->Execute("select TermID, TermReferenceID, PageNum, ParagraphNo from projectmanagement.TermReferenceMapping where TermReferenceID=1");
-$i = 0;
-while($rec = $res->fetch())
-{
-  $DocID = -1;
-  for($k=0; $k<count($docs); $k++)
+  $mysql->Prepare("select distinct TermReferenceID, PageNum, ParagraphNo from projectmanagement.TermReferenceMapping where TermReferenceID=?");
+  $res = $mysql->ExecuteStatement(array($TermRefrenceID));
+  $frequency = $docs = $terms = array();
+  $i = 0;
+  while($rec = $res->fetch())
   {
-    if($docs[$k]["TermReferenceID"] == $rec["TermReferenceID"] && $docs[$k]["PageNum"] == $rec["PageNum"] && $docs[$k]["ParagraphNo"] == $rec["ParagraphNo"])
+    $docs[$i]["PageNum"] = $rec["PageNum"];
+    $docs[$i]["TermReferenceID"] = $rec["TermReferenceID"];
+    $docs[$i]["ParagraphNo"] = $rec["ParagraphNo"];
+    $i++;
+  }
+  $res = $mysql->Execute("select distinct TermID, TermTitle from projectmanagement.terms 
+  JOIN projectmanagement.TermReferenceMapping using (TermID) 
+  where TermReferenceID=1");
+  $i = 0;
+  while($rec = $res->fetch())
+  {
+    $terms[$i]["TermID"] = $rec["TermID"];
+    $terms[$i]["TermTitle"] = $rec["TermTitle"];
+    $i++;
+  }
+  $res = $mysql->Execute("select TermID, TermReferenceID, PageNum, ParagraphNo from projectmanagement.TermReferenceMapping where TermReferenceID=1");
+  $i = 0;
+  while($rec = $res->fetch())
+  {
+    $DocID = -1;
+    for($k=0; $k<count($docs); $k++)
     {
-      $DocID = $k;
-      break;
+      if($docs[$k]["TermReferenceID"] == $rec["TermReferenceID"] && $docs[$k]["PageNum"] == $rec["PageNum"] && $docs[$k]["ParagraphNo"] == $rec["ParagraphNo"])
+      {
+        $DocID = $k;
+        break;
+      }
+    }
+    if($DocID>0)
+    {
+      $frequency[$DocID][$rec["TermID"]] = 1; // binary matrix (if there is a term in a discourse we have 1 otherwise 0/null)
     }
   }
-  if($DocID>0)
+  
+  // TermsCoOccure
+  $TermsCount = count($terms);
+  //$TermsCount = 10;
+  for($i=0; $i<$TermsCount; $i++)
   {
-    $frequency[$DocID][$rec["TermID"]] = 1; // binary matrix (if there is a term in a discourse we have 1 otherwise 0/null)
+    for($j=$i+1; $j<$TermsCount; $j++)
+    {
+      $s = 0;
+      for($DocID=0; $DocID<count($docs); $DocID++)
+      {
+        $term1 = $terms[$i]["TermID"];
+        $term2 = $terms[$j]["TermID"];
+        if(isset($frequency[$DocID][$term1]) && isset($frequency[$DocID][$term2]))
+    $s++;
+      }
+      if($s>0)
+      {
+        $mysql->Execute("insert into projectmanagement.TermsCoOccure (TermID1, TermID2, frequency) values ('".$term1."','".$term2."','".$s."')");
+        echo "'".$terms[$i]["TermTitle"]."','".$terms[$j]["TermTitle"]."','".$s."'<br>";
+      }
+    }
   }
+  
 }
 
-// TermsCoOccure
+$mysql = pdodb::getInstance();
 $mysql->Execute("delete from projectmanagement.TermsCoOccure");
-$TermsCount = count($terms);
-//$TermsCount = 10;
-for($i=0; $i<$TermsCount; $i++)
+$mysql->Prepare("select * from projectmanagement.TermReferences");
+$res = $mysql->ExecuteStatement(array());
+while($rec = $res->fetch())
 {
-  for($j=$i+1; $j<$TermsCount; $j++)
-  {
-    $s = 0;
-    for($DocID=0; $DocID<count($docs); $DocID++)
-    {
-      $term1 = $terms[$i]["TermID"];
-      $term2 = $terms[$j]["TermID"];
-      if(isset($frequency[$DocID][$term1]) && isset($frequency[$DocID][$term2]))
-	$s++;
-    }
-    if($s>0)
-    {
-      $mysql->Execute("insert into projectmanagement.TermsCoOccure (TermID1, TermID2, frequency) values ('".$term1."','".$term2."','".$s."')");
-      echo "'".$terms[$i]["TermTitle"]."','".$terms[$j]["TermTitle"]."','".$s."'<br>";
-    }
-  }
+  Calculate($mysql, $rec["TermReferenceID"]);
 }
+
+
 
 /*
 for($i=0; $i<count($docs); $i++)
